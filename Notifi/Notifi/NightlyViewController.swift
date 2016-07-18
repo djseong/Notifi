394fb454169c81ce74dc2b312777a5964ad96646
David Xu <xsq4525@126.com>
2016-07-18 14:43:55 +0200

    add init nightly view controller

---
+++ Notifi/Notifi/NightlyViewController.swift
@@ -0,0 +1,24 @@
+//
+//  NightlyViewController.swift
+//  Notifi_siqingXu
+//
+//  Created by David Xu on 7/18/16.
+//  Copyright © 2016 David Xu. All rights reserved.
+
+import UIKit
+
+class NightlyViewController: UIViewController {
+
+    @IBOutlet weak var safeButton: UIButton!
+    @IBOutlet weak var attentionButton: UIButton!
+    @IBOutlet weak var helpButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let wholeHeight = (self.view.frame.height - self.navigationController?.navigationBar.frame.height) - 
+        let safeY = self.navigationController?.navigationBar.frame.height
+        //safeButton.frame = CGRect(x: 0, y: safeY, width: self.view.width, height: 50)
+
+       
+    }
+
+}
