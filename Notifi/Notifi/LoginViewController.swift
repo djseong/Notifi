72d18ec90d69014e98e1f1ee7df19ff7ec1bb771
Daniel Seong <dseong01@college.harvard.edu>
2016-07-18 11:19:52 +0200

    git ignore

---
+++ Notifi/Notifi/LoginViewController.swift
@@ -0,0 +1,92 @@
+//
+//  LoginViewController.swift
+//  Notifi
+//
+//  Created by Daniel Seong on 7/18/16.
+//  Copyright © 2016 Daniel Seong. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKShareKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+            // Get List Of Friends
+            let friendRequest = FBSDKGraphRequest(graphPath: "me/invitable_friends", parameters: ["fields" : "id, name, picture", "after" : "QVZAtaWpPMlg3M2FHYTNEQmwwcVl6eUp5XzRNYXlGaUJZAR01KZAnBpREpHV0FJM2tTUjVYWHg0cWE2WnFYUGZA4YVYtYVU1bHJoN2RISDZAkU2REZAEY3dENzcU9FLVFVaF94eGZAKY1J4TVMxc3YwZA3cZD"], HTTPMethod: "GET")
+            friendRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+                print(result)
+
+            })
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            loginView.center = self.view.center
+            self.view.addSubview(loginView)
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            returnUserData()
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
+
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "email"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                print("User Email is: \(userEmail)")
+            }
+        })
+    }
+
+}
---
+++ Notifi/Pods/Target Support Files/Pods-Notifi/Pods-Notifi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Notifi : NSObject
+@end
+@implementation PodsDummy_Pods_Notifi
+@end
