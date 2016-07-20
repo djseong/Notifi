e98a69b3891b386fc832131309589027f1bc73e7
Daniel Seong <dseong01@college.harvard.edu>
2016-07-20 22:28:22 +0200

    new token

--- Notifi/push.py
+++ Notifi/push.py
@@ -11,21 +11,19 @@ apns = APNs(use_sandbox=True, cert_file=cert_path, key_file='')
 
 token_julz = "4a584c09cc8a7292558bcccbd58b5db47a4d1cf90908c8dab23133083f5fc8b1"
 token_david = "adb3e77d04e982b7251f020d69dccbcdf4e8d1b05988b72c0737741b50c53279"
-token_daniel = "fef69f624511f4735d4753966c808a9c4ab2055572b13720dddc000955f96b57"
+token_daniel = "ebcec145d11a072b2f72c5cfd322651f35652e0704f4af471e7895b457bf8bd2"
 
           
 successPackage = {}
-successPackage["alert"] = "TRANSACTION_SUCCESS"
-successPackage["discountAmountProcessed"] = 5000
+successPackage["alert"] = "kkk"
+successPackage["category"] = "COUNTER_CATEGORY"
 successPackage["cardAmountProcessed"] = 100
 successPackage["rewardsAmountProcessed"] = 7000
 aps = {"aps":successPackage}
 
-failurePackage = {}
-failurePackage["notificationType"] = "TRANSACTION_FAILURE"
 
 
-alert = PayloadAlert("New notification", action_loc_key = "Click me")
-payload = Payload(alert=alert, custom=aps)
+# alert = PayloadAlert("New notification", action_loc_key = "Click me")
+payload = Payload(alert="hi julian", custom=aps)
 result = apns.gateway_server.send_notification(token_daniel, payload)
 
