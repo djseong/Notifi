cc8d07959051736697cd451e9a924f5c0848c5be
Julian Hulme <julian@arcadesoftware.io>
2016-07-21 13:59:18 +0200

    handle notification

--- Notifi/Notifi/AppDelegate.swift
+++ Notifi/Notifi/AppDelegate.swift
@@ -40,10 +40,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.makeKeyAndVisible()
         
         
-
-        
         FIRApp.configure()
 
+        let refreshedToken = FIRInstanceID.instanceID().token()
+        print("InstanceID token: \(refreshedToken)")
+        
         
         initNotificationSettings()
         // Add observer for InstanceID token refresh callback.
@@ -199,12 +200,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         //first detect if the notification provided a uuid for a sensor to be alarmed.
         let aps = userInfo["aps"]
         print ("aps \(userInfo)")
-        if let alarmedUuid = aps!["uuid"]  {
-            print ("uuid in question: |\(alarmedUuid)|")
-            //go to the home screen, where the sensors are displayed, and refresh emmediately. The state of alarm is fetched along with the other information about the sensors
-            
-            
-        }
+//        if let alarmedUuid = aps!["uuid"]  {
+//            print ("uuid in question: |\(alarmedUuid)|")
+//            //go to the home screen, where the sensors are displayed, and refresh emmediately. The state of alarm is fetched along with the other information about the sensors
+//            
+//            
+//        }
         
     }
     
@@ -244,6 +245,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 print("Unable to connect with FCM. \(error)")
             } else {
                 print("Connected to FCM.")
+                
+//                SignifyUserController.sharedInstance.sendNote([""], alert:"alert", key:"")
+                SignifyUserController.sharedInstance.send()
             }
             
             
@@ -251,7 +255,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
-
-
 }
 
--- Notifi/Notifi/NightlyViewController.swift
+++ Notifi/Notifi/NightlyViewController.swift
@@ -14,7 +14,7 @@ class NightlyViewController: UIViewController {
     @IBOutlet weak var safeButton: UIButton!
     @IBOutlet weak var attentionButton: UIButton!
     @IBOutlet weak var helpButton: UIButton!
-    let message = FIRMessaging()
+    
 
     let topLabel = UILabel()
     override func viewDidLoad() {
@@ -65,7 +65,7 @@ class NightlyViewController: UIViewController {
     @IBAction func safePressed(sender: UIButton) {
         StatusController.sharedInstance.changeCurrentState(State.Safe)
         self.presentViewController(NoticeViewController(), animated: true, completion: nil)
-        message.sendMessage("fffff", to: FIRInstanceID.instanceID().token()+"gcm.google.apis.com", withMessageID: "l", timeToLive: 1)
+        
         
     }
     @IBAction func attentionPressed(sender: UIButton) {
